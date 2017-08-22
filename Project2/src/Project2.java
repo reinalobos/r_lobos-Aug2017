89628aa7fb95385e2099973011fe479692ef3565
reinalobos <reinalovelobos@gmail.com>
2017-08-22 09:28:02 -0700

    reina

---
+++ Project2/src/Project2.java
@@ -0,0 +1,15 @@
+
+// Reina Lobos
+// My second java project. It will compare print and println.
+// Aug 22, 2017
+
+public class Project2 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello--this is project 2");
+	
+		System.out.print("Hello--this is project 2");
+		
+	}
+
+}
