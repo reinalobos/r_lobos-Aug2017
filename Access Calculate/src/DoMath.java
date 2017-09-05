13995c5d645409fe286d08fe8108f5de587f9fd9
reinalobos <reinalovelobos@gmail.com>
2017-09-05 09:26:38 -0700

    homegrown library w/ math methods

---
+++ Access Calculate/src/DoMath.java
@@ -0,0 +1,11 @@
+/*Reina Lobos
+ * Sept 5, 2017
+ * runner class for my calculate library methods
+ */
+public class DoMath {
+	
+	public static void main(String[] args) {
+		Calculate.square(5);
+		System.out.println(Calculate.square(-5));
+	}
+}
---
+++ Calculate Library/Calculate.java
@@ -0,0 +1,39 @@
+/*Reina Lobos
+* Sept 5, 2017 
+*home grown library that contains various mathematical methods
+*/
+public class Calculate {
+	
+	public static int square (int operand) {
+		return operand * operand;
+/*This method takes an integer and returns its square*/
+	} 
+	public static int cube (int number) {
+		return number * number *number;
+	}
+	public static double averageOf2 (double inputNumber1, double inputnumber2) {
+		return (inputNumber1 + inputnumber2 )/2;
+	}
+	public static double averageOf3 (double inputNumber3, double inputNumber4, double inputNumber5) {
+		return ( inputNumber3 + inputNumber4 + inputNumber5 )/2;
+	}
+	public static double toDegrees (double inputRadian) {
+		return inputRadian * (180/3.14159); 
+	}
+	public static double toRadians (double inputDegrees) {
+		return inputDegrees * (3.14159/180); 
+	}
+	public static double discriminant (double a, double b, double c) {
+		return (b * b) - (4 * a * c);
+	}
+	public static int 
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
