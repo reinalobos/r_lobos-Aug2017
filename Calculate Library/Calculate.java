dfa25ab4e3c29ffa70d9b03fa8b6afec247d7b5e
reinalobos <reinalovelobos@gmail.com>
2017-09-07 08:52:33 -0700

    still needs part 2

--- Calculate Library/Calculate.java
+++ Calculate Library/Calculate.java
@@ -11,10 +11,10 @@ public class Calculate {
 	public static int cube (int number) {
 		return number * number *number;
 	}
-	public static double averageOf2 (double inputNumber1, double inputnumber2) {
+	public static double average (double inputNumber1, double inputnumber2) {
 		return (inputNumber1 + inputnumber2 )/2;
 	}
-	public static double averageOf3 (double inputNumber3, double inputNumber4, double inputNumber5) {
+	public static double average (double inputNumber3, double inputNumber4, double inputNumber5) {
 		return ( inputNumber3 + inputNumber4 + inputNumber5 )/2;
 	}
 	public static double toDegrees (double inputRadian) {
@@ -26,21 +26,23 @@ public class Calculate {
 	public static double discriminant (double a, double b, double c) {
 		return (b * b) - (4 * a * c);
 	}
-	public static int toImproperFrac (int wholeNumber, int numerator, int denominator) {
-		return (wholeNumber + numerator) / denominator;
-				// fix so it returns a string
+	public static String toImproperFrac (int wholeNumber, int numerator, int denominator) {
+		int improperFrac = (wholeNumber + numerator) / denominator;
+		return ("int improperFrac"); 
+		// fix so  it returns a string
 	}
 	public static int toMixedNum (int wholeNumber2, int numerator2, int denominator2) {
 		return 
 		// fix so it returns a string
 	}
-	public static int foil (int inputNumber6, int inputNumber7, int inputNumber8, int inputNumber9,char n) {
+	public static String foil (int inputNumber6, int inputNumber7, int inputNumber8, int inputNumber9,char n) {
 		int a2 = (inputNumber6 * inputNumber8 * n * n);
 		int b2 = (inputNumber7 * inputNumber8 * n) + (inputNumber6 * inputNumber9 * n);
 		int c2 = (inputNumber7 * inputNumber9);
-		return (a2 + b2 + c2);
+		return ("a2 + b2 + c2");
 				// not sure if this actually returns a string
 	}
+	public static int isDivisibleBy (int )
 }
 
 	
