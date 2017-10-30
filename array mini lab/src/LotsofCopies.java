fba5c24dcb59c27ab70158d817fb3377c762da0e
reinalobos <reinalovelobos@gmail.com>
2017-10-30 08:51:04 -0700

    10/30

---
+++ array mini lab/src/LotsofCopies.java
@@ -0,0 +1,13 @@
+/* Reina Lobos
+* Oct 30, 2017
+*/
+import java.util.*;
+public class LotsofCopies {
+	public static void main (String[] args) {
+		int x = 7;
+		String str = "APCS";
+	}
+	public static void changeMe (int x, String str, int[] arr) {
+		
+	}
+}
