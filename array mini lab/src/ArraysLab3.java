39dfeaf9551ee0ef80ee078aaa998e3f805c3627
reinalobos <reinalovelobos@gmail.com>
2018-02-02 08:46:26 -0800

    hobbit 2/2

--- array mini lab/src/ArraysLab3.java
+++ array mini lab/src/ArraysLab3.java
@@ -91,11 +91,24 @@ public class ArraysLab3 {
 	a1
 	*/
 
-	public static void main(String[] args) 
-	{
-
+	public static void main(String[] args) {
+		int[] a1 = {5, 10, 15, 20 , 25, 30, 35, 40};
+		int[] a2 = {7, 14, 21, 28, 35, 42, 49, 56};
+		int sumArr[] = sum(a1, a2);
+		int appendNum = 200;
+		int appendArr[] = append(a1, appendNum);
+		int removeIdx = 5;
+		int removeArr[] = remove(a2, removeIdx);
+		int sumOfEvens = sumEven(appendArr);
+		rotateRight(a1);
+		System.out.println(Arrays.toString(sumArr));
+		System.out.println(Arrays.toString(appendArr));
+		System.out.println(Arrays.toString(removeArr));
+		System.out.println(sumOfEvens);
+		System.out.println(Arrays.toString(a1));
 	}
-	}
+	
+}
 
 
 
